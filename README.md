a0ab763cc526cca2a8114f40a653f67f72a48831
xaramillo <luis.jaramillo@solariabiodata.com.mx>
2024-07-06 10:48:08 -0600

    "Primer commit"

---
+++ README.md
@@ -0,0 +1,3 @@
+# Repositorio Local
+
+Este es un código que originalmente se hizo en un repo local y se subió a Github
---
+++ condicionales.py
@@ -0,0 +1,9 @@
+# ejercicio de la semana 05
+x = int(input('ingresa un número: '))
+# 05
+# 
+# x = int( x.strip() ) # .strip() Remueve espacios, int() convierte a número entero
+if x > 10:
+    print('x es mayor a 10')
+else:
+    print('x es menor a 10')
